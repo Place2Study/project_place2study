3edc57b23d578c5166f16fcc34848ae09d815e74
Place 2 Study <place2studypdi@gmail.com>
2022-06-24 15:56:33 +0100

    new updates 24/06/2022

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -35,7 +35,7 @@ class _HomePageState extends State<HomePage> {
             automaticallyImplyLeading: true,
             flexibleSpace: FlexibleSpaceBar(
               background: Image.network(
-                'https://vui.unsplash.com/resize?height=256&quality=60&type=auto&url=https%3A%2F%2Fsearched-images.s3.us-west-2.amazonaws.com%2F4206cd61-791e-430a-b73d-922554f77a42%3FX-Amz-Algorithm%3DAWS4-HMAC-SHA256%26X-Amz-Credential%3DAKIAQ4GRIA4Q6KECABEK%252F20220622%252Fus-west-2%252Fs3%252Faws4_request%26X-Amz-Date%3D20220622T170109Z%26X-Amz-Expires%3D86400%26X-Amz-SignedHeaders%3Dhost%26X-Amz-Signature%3D8dac8a4e95860a25957f5b01621b7728d70383d731c5b5f4d6ec2a9a77107fb7&sign=52EN9q-y252pc1VX8L_jRRR59IJ_4MAOd0rYtW1-vA0',
+                'https://vui.unsplash.com/resize?height=256&quality=60&type=auto&url=https%3A%2F%2Fsearched-images.s3.us-west-2.amazonaws.com%2F9f162647-590b-410c-b4e5-d9d59f40d61e%3FX-Amz-Algorithm%3DAWS4-HMAC-SHA256%26X-Amz-Credential%3DAKIAQ4GRIA4Q6KECABEK%252F20220624%252Fus-west-2%252Fs3%252Faws4_request%26X-Amz-Date%3D20220624T145533Z%26X-Amz-Expires%3D86400%26X-Amz-SignedHeaders%3Dhost%26X-Amz-Signature%3D499c3a7cc17a8a069df4808aba64f3d5e4686ff6dffb23c24067737dcdb80bc0&sign=VW9eRHT5W_g5b3zuZRGW0PDI5ljgIotueVFFtBO2Ckg',
                 fit: BoxFit.cover,
               ),
             ),
